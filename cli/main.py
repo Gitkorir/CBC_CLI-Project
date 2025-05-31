f16fba81bd948a8e217cce56f4db1dda263a77df
Arnold <arnoldkorir201@gmail.com>
2025-05-31 13:46:44 +0300

    add a function that collects patient info and cross cheks whether its an existing patient if not adds the data to the db

--- cli/main.py
+++ cli/main.py
@@ -19,3 +19,24 @@ def list_samples():
 if __name__ == "__main__":
     cli()
 
+def add_sample():
+    """ Add a new CBC sample interactively."""
+
+    from datetime import datetime
+    from models.db_models import Sample,CBCResult,AnalysisLog
+    from database.db_session import session
+
+    #collect basic patient info
+    sample_id = click.prompt("Enter sample ID")
+    existing = session.query(Sample).filter_by(sample_id = sample_id).first()
+    if existing:
+        click.echo(f"\u26A0\uFE0F Sample with ID '(sample_id)' already Exists.")
+        return
+    
+    patient_name = click.prompt("Enter patient name")
+
+    sample = Sample(
+        sample_id = sample_id,
+        patient_name = patient_name,
+        date_collected = datetime.utcnow()
+    )
